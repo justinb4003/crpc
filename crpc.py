7b2d6dfa3e68b33fb2263fd9c94b6196f8b2645a
Justin Buist <jbuist@debt480s.gigabuist.lan>
2023-01-19 19:11:12 -0500

    Added more sizes

--- crpc.py
+++ crpc.py
@@ -17,7 +17,6 @@ fan_plate = (
 topface = fan_plate.faces(">Z")
 fan_plate = fan_plate.faces(">Z").edges("not (<X or >X or <Y or >Y)").chamfer(3)
 
-
 rib_width = 4
 rib_depth = 15
 total_y = fan_width + filter_width*2
@@ -46,59 +45,55 @@ screw_holes = (
 fan_plate = fan_plate.cut(screw_holes)
 
 fan_plate = fan_plate.faces("<Z").circle(50).cutThruAll()
+cq.exporters.export(fan_plate, f'output/cr_fanplate_{fan_width}mm.stl')
+cq.exporters.export(fan_plate, f'output/cr_fanplate_{fan_width}mm.step')
 
 
-nofan_width = 100
-nofan_plate = (
-    cq.Workplane("XY")
-        .rect(100, fan_width + filter_width*2)
-        .extrude(3)
-        .faces(">Z")
-        .center(0, total_y/2)
-        .rect(nofan_width, rib_width).extrude(rib_depth)
-        .center(0, -(filter_width+rib_width))
-        .rect(nofan_width-0, rib_width).extrude(rib_depth)
-        .center(0, -(fan_width-rib_width*2))
-        .rect(nofan_width-0, rib_width).extrude(rib_depth)
-        .center(0, -(filter_width+rib_width))
-        .rect(nofan_width, rib_width).extrude(rib_depth)
-)
-nofan_plate = nofan_plate.faces("<Z").edges("|X").fillet(1.5)
-nofan_plate = nofan_plate.faces(">Z").edges("|X").fillet(1.5)
+for nofan_width in [25, 100]:
+    nofan_plate = (
+        cq.Workplane("XY")
+            .rect(100, fan_width + filter_width*2)
+            .extrude(3)
+            .faces(">Z")
+            .center(0, total_y/2)
+            .rect(nofan_width, rib_width).extrude(rib_depth)
+            .center(0, -(filter_width+rib_width))
+            .rect(nofan_width-0, rib_width).extrude(rib_depth)
+            .center(0, -(fan_width-rib_width*2))
+            .rect(nofan_width-0, rib_width).extrude(rib_depth)
+            .center(0, -(filter_width+rib_width))
+            .rect(nofan_width, rib_width).extrude(rib_depth)
+    )
+    nofan_plate = nofan_plate.faces("<Z").edges("|X").fillet(1.5)
+    nofan_plate = nofan_plate.faces(">Z").edges("|X").fillet(1.5)
+    cq.exporters.export(nofan_plate, f'output/cr_nofanplate_{nofan_width}mm.stl')
+    cq.exporters.export(nofan_plate, f'output/cr_nofanplate_{nofan_width}mm.step')
 
-corner_width = 25 + 40
-corner_plate = (
-    cq.Workplane("XY")
-        .rect(corner_width, fan_width + filter_width*2)
-        .extrude(3)
-        .faces(">Z")
-        .center(0, total_y/2)
-        .rect(corner_width, rib_width).extrude(rib_depth)
-        .center(0, -(filter_width+rib_width))
-        .rect(corner_width-0, rib_width).extrude(rib_depth)
-        .center(0, -(fan_width-rib_width*2))
-        .rect(corner_width-0, rib_width).extrude(rib_depth)
-        .center(0, -(filter_width+rib_width))
-        .rect(corner_width, rib_width).extrude(rib_depth)
-)
-corner_plate = corner_plate.faces("<Z").edges("|X").fillet(1.5)
-corner_plate = corner_plate.faces(">Z").edges("|X").fillet(1.5)
-corner_plate = corner_plate.faces("<X").edges("|Z").fillet(1.5)
+for corner_width in [25, 65, 100]:
+    corner_plate = (
+        cq.Workplane("XY")
+            .rect(corner_width, fan_width + filter_width*2)
+            .extrude(3)
+            .faces(">Z")
+            .center(0, total_y/2)
+            .rect(corner_width, rib_width).extrude(rib_depth)
+            .center(0, -(filter_width+rib_width))
+            .rect(corner_width-0, rib_width).extrude(rib_depth)
+            .center(0, -(fan_width-rib_width*2))
+            .rect(corner_width-0, rib_width).extrude(rib_depth)
+            .center(0, -(filter_width+rib_width))
+            .rect(corner_width, rib_width).extrude(rib_depth)
+    )
+    corner_plate = corner_plate.faces("<Z").edges("|X").fillet(1.5)
+    corner_plate = corner_plate.faces(">Z").edges("|X").fillet(1.5)
+    corner_plate = corner_plate.faces("<X").edges("|Z").fillet(1.5)
 
-corner = (
-    corner_plate
-    .mirror(mirrorPlane="YZ", basePointVector=(0, 0, 0))
-    .rotate((0, 0, 0), (0, 1, 0), 90)
-    .translate((-corner_width/2, 0, corner_width/2))
-)
-# corner = corner.edges("<Y and <Z").fillet(1.5)
-corner_plate = corner_plate.union(corner)
-
-
-cq.exporters.export(fan_plate, 'output/cr_fanplate.stl')
-cq.exporters.export(nofan_plate, 'output/cr_nofanplate.stl')
-cq.exporters.export(corner_plate, 'output/cr_corner.stl')
-
-cq.exporters.export(fan_plate, 'output/cr_fanplate.step')
-cq.exporters.export(nofan_plate, 'output/cr_nofanplate.step')
-cq.exporters.export(corner_plate, 'output/cr_corner.step')
+    corner = (
+        corner_plate
+        .mirror(mirrorPlane="YZ", basePointVector=(0, 0, 0))
+        .rotate((0, 0, 0), (0, 1, 0), 90)
+        .translate((-corner_width/2, 0, corner_width/2))
+    )
+    corner_plate = corner_plate.union(corner)
+    cq.exporters.export(corner_plate, f'output/cr_corner_{corner_width}mm.stl')
+    cq.exporters.export(corner_plate, f'output/cr_corner_{corner_width}mm.step')
